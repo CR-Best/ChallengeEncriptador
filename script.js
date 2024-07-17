7b370cc47ffeae1830b8e1fabcf60ec6455a0780
CR-Best <crosales3103@gmail.com>
2024-07-17 09:22:30 -0600

    Se comienza a escribir el progtrama en script.js y se trabajara con arrays y expresiones regulares

---
+++ script.js
@@ -0,0 +1,11 @@
+//Arrays con las reglas de encriptacion y desencriptacion
+let letraOriginal = ['e', 'i', 'a', 'o', 'u'];
+let letraEncriptada = ['enter', 'imes', 'ai', 'ober', 'ufat'];
+
+//Funcion para encriptar el texto usando arrays
+function encriparTexto(texto){
+    let textoEncriptado = texto;
+    for(let i = 0; i < letraOriginal.length; i++){
+        let expresionRegular = new RegExp
+    }
+}
